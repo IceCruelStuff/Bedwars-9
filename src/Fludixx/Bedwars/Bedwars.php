998cf6b0d627022ffdbc92480613eb2d30c24df0
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-11 12:30:21 -0700

    Update Bedwars.php

--- src/Fludixx/Bedwars/Bedwars.php
+++ src/Fludixx/Bedwars/Bedwars.php
@@ -42,50 +42,55 @@ use pocketmine\utils\Config;
  * @package Fludixx\Bedwars
  * This is the Main class this class sets everything up, it loads and converts arenas into an Arena object (@see Arena.php), for example
  */
-class Bedwars extends PluginBase {
+class Bedwars extends PluginBase
+{
 
-	const NAME    = "§c- Bedwars -";
-	const PREFIX  = "§7[§cBedwars§7] §f";
-	const JOIN    = "§a[JOIN]";
-	const FULL    = "§c[FULL]";
-	const RUNNING = "§7[SPECTATE]";
-	const BLOCKS = [ // Breakable blocks
-		Block::SANDSTONE, Block::END_STONE, Block::GLASS,
-		Block::CHEST, Block::IRON_BLOCK, Block::COBWEB
-	];
-
-	/** @var Bedwars */
-	private static $instance;
-	/** @var ProviderInterface */
-	public static $provider;
-	/** @var BWPlayer[] */
-	public static $players = [];
-	/** @var Arena[] */
-	public static $arenas = [];
-	public static $mysqlLogin = [];
-	/** @var StatsInterface */
-	public static $statsSystem;
-	private $settings = [
-	    'stats' => 'json'
+    const NAME = "§c- Bedwars -";
+    const PREFIX = "§7[§cBedwars§7] §f";
+    const JOIN = "§a[JOIN]";
+    const FULL = "§c[FULL]";
+    const RUNNING = "§7[SPECTATE]";
+    const BLOCKS = [ // Breakable blocks
+        Block::SANDSTONE,
+        Block::END_STONE,
+        Block::GLASS,
+        Block::CHEST,
+        Block::IRON_BLOCK,
+        Block::COBWEB
     ];
 
-	public function onEnable()
-	{
+    /** @var Bedwars */
+    private static $instance;
+    /** @var ProviderInterface */
+    public static $provider;
+    /** @var BWPlayer[] */
+    public static $players = [];
+    /** @var Arena[] */
+    public static $arenas = [];
+    public static $mysqlLogin = [];
+    /** @var StatsInterface */
+    public static $statsSystem;
+    private $settings = [
+        'stats' => 'json'
+    ];
+
+    public function onEnable()
+    {
         self::$instance = $this;
-		@mkdir($this->getDataFolder());
-		if(!file_exists($this->getDataFolder()."/mysql.yml")) {
+        @mkdir($this->getDataFolder());
+        if(!file_exists($this->getDataFolder()."/mysql.yml")) {
             $mysql = new Config($this->getDataFolder()."/mysql.yml", Config::YAML);
-		    $mysql->setAll([
-		        'host' => '127.0.0.1',
+            $mysql->setAll([
+                'host' => '127.0.0.1',
                 'user' => 'admin',
                 'pass' => 'admin',
                 'db'   => 'bwStats'
             ]);
-		    $mysql->save();
+            $mysql->save();
         }
         $mysql = new Config($this->getDataFolder()."/mysql.yml", Config::YAML);
-		self::$mysqlLogin = $mysql->getAll();
-		switch ($this->settings['stats']) {
+        self::$mysqlLogin = $mysql->getAll();
+        switch ($this->settings['stats']) {
             case 'mysql':
                 // TODO make mysql stats
                 //self::$statsSystem = new MySqlStats();
@@ -93,48 +98,52 @@ class Bedwars extends PluginBase {
             default:
                 self::$statsSystem = new JsonStats();
         }
-		if(!$this->getServer()->loadLevel("transfare"))
-			$this->getServer()->generateLevel("transfare");
-		self::$provider = new JsonProvider();
-		$this->registerCommands();
-		$this->registerEvents();
-		$this->loadArenas();
-		$this->getScheduler()->scheduleRepeatingTask(new BWTask(), 20);
-		$this->getScheduler()->scheduleRepeatingTask(new SignTask(), 20);
-		$this->getLogger()->info(self::PREFIX."Bedwars geladen");
+        if(!$this->getServer()->loadLevel("transfare"))
+            $this->getServer()->generateLevel("transfare");
+        self::$provider = new JsonProvider();
+        $this->registerCommands();
+        $this->registerEvents();
+        $this->loadArenas();
+        $this->getScheduler()->scheduleRepeatingTask(new BWTask(), 20);
+        $this->getScheduler()->scheduleRepeatingTask(new SignTask(), 20);
+        $this->getLogger()->info(self::PREFIX."Bedwars geladen");
         if (!InvMenuHandler::isRegistered())
             InvMenuHandler::register(Bedwars::getInstance());
-	}
+    }
 
-    private function registerEvents() {
-		$pm = $this->getServer()->getPluginManager();
-		$pm->registerEvents(new PlayerJoinListener(), $this);
-		$pm->registerEvents(new EntityDamageListener(), $this);
-		$pm->registerEvents(new BlockEventListener(), $this);
-		$pm->registerEvents(new InteractListener(), $this);
-		$pm->registerEvents(new ChatListener(), $this);
-	}
+    private function registerEvents()
+    {
+        $pm = $this->getServer()->getPluginManager();
+        $pm->registerEvents(new PlayerJoinListener(), $this);
+        $pm->registerEvents(new EntityDamageListener(), $this);
+        $pm->registerEvents(new BlockEventListener(), $this);
+        $pm->registerEvents(new InteractListener(), $this);
+        $pm->registerEvents(new ChatListener(), $this);
+    }
 
-	private function registerCommands() {
-		$map = $this->getServer()->getCommandMap();
-		$map->register("bw", new bedwarsCommand());
-		$map->register("leave", new leaveCommand());
-		$map->register("sign", new SignCommand());
-		$map->register("start", new StartCommand());
-		$map->register("stats", new viewStatsCommand());
-		$map->register("bwbuild", new BuildCommand());
-	}
+    private function registerCommands()
+    {
+        $map = $this->getServer()->getCommandMap();
+        $map->register("bw", new bedwarsCommand());
+        $map->register("leave", new leaveCommand());
+        $map->register("sign", new SignCommand());
+        $map->register("start", new StartCommand());
+        $map->register("stats", new viewStatsCommand());
+        $map->register("bwbuild", new BuildCommand());
+    }
 
-	private function loadArenas() {
-		foreach (self::$provider->getArenas() as $name => $data) {
-			$this->getServer()->loadLevel($data['mapname']);
-			$level = $this->getServer()->getLevelByName($data['mapname']);
+    private function loadArenas()
+    {
+        foreach (self::$provider->getArenas() as $name => $data) {
+            $this->getServer()->loadLevel($data['mapname']);
+            $level = $this->getServer()->getLevelByName($data['mapname']);
             self::$arenas[$name] = new Arena($data['mapname'], (int)$data['ppt'], (int)$data['teams'], $level, $data['spawns']);
-		}
-	}
+        }
+    }
 
-	public static function saveStats() {
-	    // TODO: Rewrite stats system
+    public static function saveStats()
+    {
+        // TODO: Rewrite stats system
     }
 
     public function onDisable()
@@ -142,8 +151,9 @@ class Bedwars extends PluginBase {
         self::saveStats();
     }
 
-    public static function getInstance() : Bedwars {
-		return self::$instance;
-	}
+    public static function getInstance() : Bedwars
+    {
+        return self::$instance;
+    }
 
 }
