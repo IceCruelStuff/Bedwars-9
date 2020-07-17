71dcb0545ee69d3432a92e51bdcbf8ad464bb043
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-16 20:29:30 -0700

    Update MenuIds.php

--- src/muqsit/invmenu/MenuIds.php
+++ src/muqsit/invmenu/MenuIds.php
@@ -23,12 +23,12 @@ use muqsit\invmenu\inventories\ChestInventory;
 use muqsit\invmenu\inventories\DoubleChestInventory;
 use muqsit\invmenu\inventories\HopperInventory;
 
-interface MenuIds{
+interface MenuIds {
 
-	// This interface exists for backwards compatibility.
+    // This interface exists for backwards compatibility.
 
-	const TYPE_CHEST = ChestInventory::class;
-	const TYPE_DOUBLE_CHEST = DoubleChestInventory::class;
-	const TYPE_HOPPER = HopperInventory::class;
+    const TYPE_CHEST = ChestInventory::class;
+    const TYPE_DOUBLE_CHEST = DoubleChestInventory::class;
+    const TYPE_HOPPER = HopperInventory::class;
 
 }
