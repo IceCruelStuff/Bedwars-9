205ddee3cecb0e73f32fc95353bbcd1aa4b715e9
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-16 20:37:29 -0700

    Update HopperInventory.php

--- src/muqsit/invmenu/inventories/HopperInventory.php
+++ src/muqsit/invmenu/inventories/HopperInventory.php
@@ -22,25 +22,31 @@ namespace muqsit\invmenu\inventories;
 use pocketmine\block\Block;
 use pocketmine\network\mcpe\protocol\types\WindowTypes;
 
-class HopperInventory extends SingleBlockInventory{
+class HopperInventory extends SingleBlockInventory {
 
-	public function getBlock() : Block{
-		return Block::get(Block::HOPPER_BLOCK);
-	}
+    public function getBlock() : Block
+    {
+        return Block::get(Block::HOPPER_BLOCK);
+    }
 
-	public function getNetworkType() : int{
-		return WindowTypes::HOPPER;
-	}
+    public function getNetworkType() : int
+    {
+        return WindowTypes::HOPPER;
+    }
 
-	public function getTileId() : string{
-		return "Hopper";
-	}
+    public function getTileId() : string
+    {
+        return "Hopper";
+    }
 
-	public function getName() : string{
-		return "Hopper";
-	}
+    public function getName() : string
+    {
+        return "Hopper";
+    }
 
-	public function getDefaultSize() : int{
-		return 5;
-	}
+    public function getDefaultSize() : int
+    {
+        return 5;
+    }
+
 }
