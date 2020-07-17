2253bd4adffe9a7cc278d97116a08c65dd4f9902
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-16 20:33:36 -0700

    Update DelayedFakeBlockDataNotifyTask.php

--- src/muqsit/invmenu/tasks/DelayedFakeBlockDataNotifyTask.php
+++ src/muqsit/invmenu/tasks/DelayedFakeBlockDataNotifyTask.php
@@ -24,24 +24,27 @@ use muqsit\invmenu\inventories\BaseFakeInventory;
 use pocketmine\Player;
 use pocketmine\scheduler\Task;
 
-class DelayedFakeBlockDataNotifyTask extends Task{
+class DelayedFakeBlockDataNotifyTask extends Task {
 
-	/** @var Player */
-	private $player;
+    /** @var Player */
+    private $player;
 
-	/** @var BaseFakeInventory */
-	private $inventory;
+    /** @var BaseFakeInventory */
+    private $inventory;
 
-	public function __construct(Player $player, BaseFakeInventory $inventory){
-		$this->player = $player;
-		$this->inventory = $inventory;
-	}
+    public function __construct(Player $player, BaseFakeInventory $inventory)
+    {
+        $this->player = $player;
+        $this->inventory = $inventory;
+    }
 
-	public function onRun(int $tick) : void{
-		if($this->player->isConnected()){
-			$this->inventory->onFakeBlockDataSendSuccess($this->player);
-		}else{
-			$this->inventory->onFakeBlockDataSendFailed($this->player);
-		}
-	}
+    public function onRun(int $tick) : void
+    {
+        if ($this->player->isConnected()) {
+            $this->inventory->onFakeBlockDataSendSuccess($this->player);
+        } else {
+            $this->inventory->onFakeBlockDataSendFailed($this->player);
+        }
+    }
+
 }
