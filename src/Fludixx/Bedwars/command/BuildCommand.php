08f5c9c541d9157e1d605bd423f76b01ff072681
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-16 20:52:42 -0700

    Update BuildCommand.php

--- src/Fludixx/Bedwars/command/BuildCommand.php
+++ src/Fludixx/Bedwars/command/BuildCommand.php
@@ -14,7 +14,8 @@ use Fludixx\Bedwars\Bedwars;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-class BuildCommand extends Command {
+class BuildCommand extends Command
+{
 
 
     public function __construct()
@@ -25,15 +26,15 @@ class BuildCommand extends Command {
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
-        if($sender->hasPermission("bw.build")) {
+        if ($sender->hasPermission("bw.build")) {
             $mplayer = Bedwars::$players[$sender->getName()];
             $mplayer->setCanBuild(!$mplayer->canBuild());
-            if($mplayer->canBuild()) {
+            if ($mplayer->canBuild()) {
                 $mplayer->sendMsg("You can now place & break blocks!");
             } else {
                 $mplayer->sendMsg("You can't build now!");
             }
-            return TRUE;
+            return true;
         } else {
             $sender->sendMessage("§cYou don't have the Permissions for this command");
         }
