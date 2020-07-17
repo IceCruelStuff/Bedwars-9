1066bfbd032b0583e4973dea4122b7ee402cdeb3
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-16 20:51:11 -0700

    Update InteractListener.php

--- src/Fludixx/Bedwars/event/InteractListener.php
+++ src/Fludixx/Bedwars/event/InteractListener.php
@@ -22,16 +22,18 @@ use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
 use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
 use pocketmine\tile\Sign;
 
-class InteractListener implements Listener {
+class InteractListener implements Listener
+{
 
-	public function onInteract(PlayerInteractEvent $event) {
-		$mplayer = Bedwars::$players[$event->getPlayer()->getName()];
-		$tile = $event->getBlock()->getLevel()->getTile($event->getBlock()->asVector3());
-		if($mplayer->getPos() === 0 and $tile instanceof Sign and $tile->getLine(0) === Bedwars::NAME) {
-			if($tile->getLine(3) === Bedwars::JOIN) {
-				$mplayer->sendMsg("Teleporting...");
-				$arena = Bedwars::$arenas[$tile->getLine(1)];
-				if(count($arena->getPlayers()) < ($arena->getTeams() * $arena->getPlayersProTeam())) {
+    public function onInteract(PlayerInteractEvent $event)
+    {
+        $mplayer = Bedwars::$players[$event->getPlayer()->getName()];
+        $tile = $event->getBlock()->getLevel()->getTile($event->getBlock()->asVector3());
+        if ($mplayer->getPos() === 0 && $tile instanceof Sign && $tile->getLine(0) === Bedwars::NAME) {
+            if ($tile->getLine(3) === Bedwars::JOIN) {
+                $mplayer->sendMsg("Teleporting...");
+                $arena = Bedwars::$arenas[$tile->getLine(1)];
+                if (count($arena->getPlayers()) < ($arena->getTeams() * $arena->getPlayersProTeam())) {
                     $randomteam = $mplayer->getRandomTeam($arena);
                     $mplayer->setTeam($randomteam);
                     $mplayer->saveTeleport($arena->getLevel()->getSafeSpawn());
@@ -43,9 +45,9 @@ class InteractListener implements Listener {
                     $arena->broadcast("{$mplayer->getName()} joined!");
                     return;
                 }
-			} else if($tile->getLine(3) === Bedwars::RUNNING or $tile->getLine(3) === Bedwars::FULL) {
+            } else if($tile->getLine(3) === Bedwars::RUNNING || $tile->getLine(3) === Bedwars::FULL) {
                 $arena = Bedwars::$arenas[$tile->getLine(1)];
-                if($arena->getState() === Arena::STATE_INUSE) {
+                if ($arena->getState() === Arena::STATE_INUSE) {
                     $mplayer->sendMsg("Teleporting...");
                     $inv = $mplayer->getPlayer()->getInventory();
                     $inv->setItem(0, Item::get(Item::SLIME_BALL)->setCustomName("§cLeave"));
@@ -54,35 +56,35 @@ class InteractListener implements Listener {
                     $mplayer->saveTeleport($arena->getLevel()->getSafeSpawn());
                 }
             }
-			$mplayer->sendMsg("You can't join this Round!");
-		}
-		switch ($event->getItem()->getCustomName()) {
-			case "§eTeams":
-				$menu = InvMenu::create(InvMenu::TYPE_CHEST);
-				$menu->readonly();
-				$menu->setName("Select your Team!");
-				$minv = $menu->getInventory();
-				$levelname = $mplayer->getPlayer()->getLevel()->getFolderName();
-				$arena = Bedwars::$arenas[$levelname];
-				$teams = [];
-				for($i = 1;$i <= $arena->getTeams();$i++) {
-				    $teams[$i] = [];
+            $mplayer->sendMsg("You can't join this Round!");
+        }
+        switch ($event->getItem()->getCustomName()) {
+            case "§eTeams":
+                $menu = InvMenu::create(InvMenu::TYPE_CHEST);
+                $menu->readonly();
+                $menu->setName("Select your Team!");
+                $minv = $menu->getInventory();
+                $levelname = $mplayer->getPlayer()->getLevel()->getFolderName();
+                $arena = Bedwars::$arenas[$levelname];
+                $teams = [];
+                for ($i = 1;$i <= $arena->getTeams();$i++) {
+                    $teams[$i] = [];
                 }
-				foreach ($arena->getLevel()->getPlayers() as $player) {
-					$mplayer = Bedwars::$players[$player->getName()];
-					$teams[$mplayer->getTeam()][] = $mplayer->getName();
-				}
-				foreach ($arena->getBeds() as $team => $bed) {
-					$playerss = "";
-					foreach ($teams[$team] as $name) {
-						$playerss .= "\n§7 - §f$name";
-					}
-					$minv->addItem(Item::get(Item::WOOL, Utils::teamIntToColorInt($team), count($teams[$team])+1)->setCustomName
-					(Utils::ColorInt2Color(Utils::teamIntToColorInt($team))."§7 - §f".count($teams[$team]).$playerss));
-				}
-				$menu->send($event->getPlayer());
-				$menu->setListener([new ChestListener(), "onSelect"]);
-				break;
+                foreach ($arena->getLevel()->getPlayers() as $player) {
+                    $mplayer = Bedwars::$players[$player->getName()];
+                    $teams[$mplayer->getTeam()][] = $mplayer->getName();
+                }
+                foreach ($arena->getBeds() as $team => $bed) {
+                    $playerss = "";
+                    foreach ($teams[$team] as $name) {
+                        $playerss .= "\n§7 - §f$name";
+                    }
+                    $minv->addItem(Item::get(Item::WOOL, Utils::teamIntToColorInt($team), count($teams[$team])+1)->setCustomName
+                    (Utils::ColorInt2Color(Utils::teamIntToColorInt($team))."§7 - §f".count($teams[$team]).$playerss));
+                }
+                $menu->send($event->getPlayer());
+                $menu->setListener([new ChestListener(), "onSelect"]);
+                break;
             case "§6Goldvote":
                 $form = new ModalFormRequestPacket();
                 $form->formId = 156;
@@ -100,21 +102,22 @@ class InteractListener implements Listener {
             case "§cLeave":
                 Bedwars::getInstance()->getServer()->dispatchCommand($event->getPlayer(), "leave");
                 break;
-		}
-	}
+        }
+    }
 
-	public function ondataPacketRecive(DataPacketReceiveEvent $event) {
-	    $p = $event->getPacket();
-	    if($p instanceof ModalFormResponsePacket and $p->formId === 156) {
-	        $action = json_decode($p->formData);
-	        $mplayer = Bedwars::$players[$event->getPlayer()->getName()];
-            if(!is_null($action)) {
+    public function ondataPacketRecive(DataPacketReceiveEvent $event)
+    {
+        $p = $event->getPacket();
+        if ($p instanceof ModalFormResponsePacket && $p->formId === 156) {
+            $action = json_decode($p->formData);
+            $mplayer = Bedwars::$players[$event->getPlayer()->getName()];
+            if (!is_null($action)) {
                 if ($action === 1) {
                     $mplayer->setForGold(FALSE);
-                    $mplayer->sendMsg("You wan't to play §cWITHOUT§f gold");
+                    $mplayer->sendMsg("You want to play §cWITHOUT§f gold");
                 } else {
                     $mplayer->setForGold(TRUE);
-                    $mplayer->sendMsg("You wan't to play §aWITH§f gold");
+                    $mplayer->sendMsg("You want to play §aWITH§f gold");
                 }
             }
         }
