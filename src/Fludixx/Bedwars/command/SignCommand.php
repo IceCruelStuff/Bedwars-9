caa5d30f7efe8641fe573da8ec5d2306642245b9
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-16 20:53:59 -0700

    Update SignCommand.php

--- src/Fludixx/Bedwars/command/SignCommand.php
+++ src/Fludixx/Bedwars/command/SignCommand.php
@@ -15,35 +15,39 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\Player;
 
-class SignCommand extends Command {
+class SignCommand extends Command
+{
 
-	public function __construct()
-	{
-		parent::__construct("sign",
-			"/sign [ARENANAME]",
-			"/sign [ARENANAME]", ['addsign']);
-	}
+    public function __construct()
+    {
+        parent::__construct(
+            "sign",
+            "/sign [ARENANAME]",
+            "/sign [ARENANAME]",
+            ['addsign']
+        );
+    }
 
-	public function execute(CommandSender $sender, string $commandLabel, array $args)
-	{
-		if($sender->hasPermission("bw.admin") and $sender instanceof Player) {
-			if(!isset($args[0]) or $args[0] == "help") {
-				$sender->sendMessage(Bedwars::PREFIX."/sign [ARENANAME]");
-				return TRUE;
-			} else {
-				$arenas = Bedwars::$provider->getArenas();
-				if(!isset($arenas[$args[0]])) {
-					$sender->sendMessage(Bedwars::PREFIX."Arena not found! Be sure to register it");
-					return FALSE;
-				} else {
-					$sender->sendMessage(Bedwars::PREFIX."Break a Sign");
-					Bedwars::$players[$sender->getName()]->setPos(-11);
-					Bedwars::$players[$sender->getName()]->setKnocker($args[0]);
-					return TRUE;
-				}
-			}
-		}
-		return FALSE;
-	}
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if ($sender->hasPermission("bw.admin") && $sender instanceof Player) {
+            if (!isset($args[0]) || $args[0] == "help") {
+                $sender->sendMessage(Bedwars::PREFIX."/sign [ARENANAME]");
+                return true;
+            } else {
+                $arenas = Bedwars::$provider->getArenas();
+                if (!isset($arenas[$args[0]])) {
+                    $sender->sendMessage(Bedwars::PREFIX."Arena not found! Be sure to register it");
+                    return false;
+                } else {
+                    $sender->sendMessage(Bedwars::PREFIX."Break a Sign");
+                    Bedwars::$players[$sender->getName()]->setPos(-11);
+                    Bedwars::$players[$sender->getName()]->setKnocker($args[0]);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
